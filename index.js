ad877e1b9e9932d620a944a11d6a913cc04c3093
Rachell Sincere <rachellsincere@gmail.com>
2019-08-07 21:33:03 -0400

    built first get request endpoint. Started Post request endpoint

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 // implement your API here
 const express = require('express');
 
+const db = require('./data/db.js');
+
 const server = express();
 
 // what happens on a POST request to /api/users
+server.post('/api/users', (req/*incoming*/, res/*outgoing*/) => {
+
+});
+
 //GET request to /api/users
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+        res.json(users);
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: "The users information could not be retrieved."
+        })
+    })
+    
+
+});
 //GET request to /api/users/:id
 //DELETE request to /api/users/:id
 //PUT request to /api/users/:id
