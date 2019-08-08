6a705c4393e44b0f5e30c10d891cdb5e11ced6dd
Rachell Sincere <rachellsincere@gmail.com>
2019-08-07 20:56:05 -0400

    built server using express. commented out the endpoint specifications that will be built in index.js. npm installed express to build the server using express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+// what happens on a POST request to /api/users
+//GET request to /api/users
+//GET request to /api/users/:id
+//DELETE request to /api/users/:id
+//PUT request to /api/users/:id
+
+
+
+//should be the last step 
+server.listen(4000, () => {
+    console.log('server is running on port 4000');
+}); 
